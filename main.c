1ad9e3e16a3535237f6d95d4453fe94c7906937d
Zirke <34094117+Zirke@users.noreply.github.com>
2017-11-30 14:48:10 +0100

    Fixed everything

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 
 int main(void){
 
-    int i;
+    int i=0;
     int k=4;
     for(i = 0; i < k; ++i){
         printf("Boch og Lisa er fagots :)\n");
-    } 
+    }
+    do{
+        printf("Farvel Lisa og Boch\n");
+        ++i;
+    }while(i < 100);
+
+
 
   return 0;
 }
