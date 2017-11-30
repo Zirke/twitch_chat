6831f84c7df970fd28f162a5efd001600bb5805a
BochAbuse <34130364+BochAbuse@users.noreply.github.com>
2017-11-30 15:03:00 +0100

    done

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main(void){
     int i=0;
     int k=4;
     for(i = 0; i < k; ++i){
-        printf("Boch og Lisa er fagots. rygere = klamme aber Fredrik De Frene er en böse:)\n");
+        printf("Boch og Lisa er okay. Fredrik De Frene er en böse:)\n");
     }
     do{
-        printf("Farvel Lisa og Boch\n");
+        printf("goddag til Lisa og Boch\n");
         ++i;
     }while(i < 100);
 
