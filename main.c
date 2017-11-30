01ed0501452855a64e0910f552a97a99b8cbd9e6
Zirke <34094117+Zirke@users.noreply.github.com>
2017-11-30 15:07:43 +0100

    Vi er færdige

--- main.c
+++ main.c
@@ -1,18 +1,61 @@
 #include <stdio.h>
 
+#define MAX_SIZE 160
+
+int countAllEntries();
+void read_data(FILE*, int, chat_entry logs[])
+
+typedef struct chat_entry{
+    char timestamp[MAX_SIZE];
+    char username[MAX_SIZE];
+    char message[MAX_SIZE];
+} chat_entry;
+
 int main(void){
 
-    int i=0;
-    int k=4;
-    for(i = 0; i < k; ++i){
-        printf("Boch og Lisa er fagots :)\n");
-    }
-    do{
-        printf("Farvel Lisa og Boch\n");
-        ++i;
-    }while(i < 100);
+    FILE *chat_log = fopen("twitchlogs.txt", "r");
 
+    int total_entries = countAllEntries();
+    chat_entry logs[total_entries];
+    read_data(total_entries,logs);
 
+    fclose(chat_log);
 
   return 0;
+}
+
+int countAllEntries(){
+
+  int  totalLines = 1;
+  char ch;
+  FILE *chat_log = fopen("twitchlogs.txt", "r");
+
+
+  while(!feof(chat_log))
+  {
+    ch = fgetc(chat_log);
+
+    if(ch == '\n') /* Increment counter when encountering newline */
+    {
+      ++totalLines;
+    }
+  }
+
+  fclose(chat_log);
+  return totalLines;
+}
+
+/* Function for reading data from file to struct */
+void read_data(FILE *chat_log, int total_entires, chat_entry logs[total_entires]){
+
+  int i;
+  chat_entry data;
+  for(i = 0; i < total_riders; ++i)
+  {
+      fscanf(results,"%s %s %s",
+            data.timestamp,
+            data.username,
+            data.message);
+    logs[i] = data; /* Transfer data from structs to array of structs */
+  }
 }
