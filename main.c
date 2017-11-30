575a0aa4411516347612844fa594df364d4db591
abiram98 <abiram98@gmail.com>
2017-11-30 14:49:54 +0100

    ag
    d

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void){
     int i=0;
     int k=4;
     for(i = 0; i < k; ++i){
-        printf("Boch og Lisa er fagots :)\n");
+        printf("Boch og Lisa er fagots. rygere = klamme aber :)\n");
     }
     do{
         printf("Farvel Lisa og Boch\n");
