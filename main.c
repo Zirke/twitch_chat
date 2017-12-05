bf621ebe8b0a208307a8d0471a35e1e923a0f0af
Zirke <34094117+Zirke@users.noreply.github.com>
2017-12-05 23:04:03 +0100

    C89 + Formatting
    Changed variable sized array to malloc and some formatting

--- main.c
+++ main.c
@@ -34,8 +34,8 @@ int main(void){
     int total_entries_wordlist = countAllEntries(user_wordlist);
     int total_entries_log = countAllEntries(chat_log);
     int user_threshold = 0;
-    wordlist words[total_entries_wordlist];
-    chat_entry logs[total_entries_log];
+    wordlist *words = malloc(sizeof(wordlist) * total_entries_wordlist);
+    chat_entry *logs = malloc(sizeof(chat_entry) * total_entries_log);
     read_wordlist(user_wordlist, total_entries_wordlist, words);
     read_data_log(chat_log, total_entries_log, logs);
 
@@ -49,6 +49,7 @@ int main(void){
 
     fclose(user_wordlist);
     fclose(chat_log);
+    free(words);
     
     
   return 0;
@@ -71,22 +72,22 @@ int countAllEntries(FILE *datafile){
 }
 
 /* Function for reading data from file to struct */
-void read_data_log(FILE *chat_log, int total_entries_log, chat_entry logs[total_entries_log]){
+void read_data_log(FILE *chat_log, int total_entries_log, chat_entry logs[]){
 
   int i;
   char temp_timestamp[MAX_SIZE];
   chat_entry data = {0};
   for(i = 0; i < total_entries_log; ++i){
       fscanf(chat_log," [%[^][]] %[^:] %*[:] %[^\n]",
-            temp_timestamp,
-            data.username,
-            data.message);
+                                      temp_timestamp,
+                                      data.username,
+                                      data.message);
       sscanf(temp_timestamp," %*s %s %*s",data.timestamp); /* Scan only hours, minutes, seconds */
     logs[i] = data; /* Transfer data from structs to array of structs */
   }
 }
 
-void read_wordlist(FILE *user_wordlist, int total_entries_wordlist, wordlist words[total_entries_wordlist]){
+void read_wordlist(FILE *user_wordlist, int total_entries_wordlist, wordlist words[]){
 
   int i;
   wordlist data = {0};
@@ -96,7 +97,7 @@ void read_wordlist(FILE *user_wordlist, int total_entries_wordlist, wordlist wor
   }
 }
 
-void assign_points(int total_entries_log, int total_entries_wordlist, chat_entry logs[total_entries_log], wordlist words[total_entries_wordlist]){
+void assign_points(int total_entries_log, int total_entries_wordlist, chat_entry logs[], wordlist words[]){
 
   int i, j;
   for (i = 0; i < total_entries_log; ++i){
@@ -108,30 +109,30 @@ void assign_points(int total_entries_log, int total_entries_wordlist, chat_entry
   }
 }
 
-void print_over_threshold(int total_entries_log, chat_entry logs[total_entries_log], int threshold){
+void print_over_threshold(int total_entries_log, chat_entry logs[], int threshold){
 
   int i;
-  chat_entry temp[total_entries_log];
+  chat_entry *temp = malloc(sizeof(chat_entry) * total_entries_log);
   for (i = 0; i < total_entries_log; ++i){
-      temp[i] = logs[i];
+    temp[i] = logs[i];
   }
   qsort(temp, total_entries_log, sizeof(chat_entry), compare_points);
 
   for (i = 0; i < total_entries_log; ++i){
     if(temp[i].points >= threshold){
-      printf("Points: %d |--| Timestamp: %s |--| Username: %25s |--| Message: %s\n",temp[i].points, temp[i].timestamp, temp[i].username, temp[i].message);
+      printf("Points: %d |--| Timestamp: %s |--| Username: %-15s |--| Message: %s\n",temp[i].points, temp[i].timestamp, temp[i].username, temp[i].message);
     }
   }
 }
 
-void check_for_question(int total_entries_log, chat_entry logs[total_entries_log], int total_entries_wordlist, wordlist words[total_entries_wordlist]){
+void check_for_question(int total_entries_log, chat_entry logs[], int total_entries_wordlist, wordlist words[]){
 
-  int i, j, k=0;
+  int i, j;
   for(i = 0; i < total_entries_log; ++i){
     for (j = 0; j < total_entries_wordlist; ++j){
       if(strstr(logs[i].message, words[j].word) != NULL){
-          printf("[%s] %s: %s\n",logs[i].timestamp,logs[i].username,logs[i].message); 
-          break;
+        printf("[%s] %s: %s\n",logs[i].timestamp,logs[i].username,logs[i].message); 
+        break;
       }
     }
   }
@@ -140,10 +141,13 @@ void check_for_question(int total_entries_log, chat_entry logs[total_entries_log
 int compare_points (const void * a, const void * b){
     chat_entry *ia = (chat_entry *)a;
     chat_entry *ib = (chat_entry *)b;
-    if(ia->points > ib->points)
-        return -1;
-    else if (ia->points < ib->points)
-        return +1;
-    else 
-        return 0;
+    if(ia->points > ib->points){
+      return -1;
+    }
+    else if (ia->points < ib->points){
+      return +1;
+    }
+    else{ 
+      return 0;
+    }
 }
